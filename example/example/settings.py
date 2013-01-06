16b207cc834439205ddeb5a3f7ac48aa9b8ab184
Alex Mavrogiannis <nalfemp@gmail.com>
2013-01-06 17:59:37 +0200

    added core app

--- example/example/settings.py
+++ example/example/settings.py
@@ -113,11 +113,11 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-#    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'spyglass',
+    'example.core',
 )
 
 # A sample logging configuration. The only tangible logging
