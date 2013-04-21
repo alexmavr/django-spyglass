85818bf8ee389b2191f8439d9301c902596ae439
Alex Mavrogiannis <nalfemp@gmail.com>
2013-04-21 17:39:34 +0300

    notifications are up

--- example/core/tables.py
+++ example/core/tables.py
@@ -1,6 +1,7 @@
 import django_tables2 as tables
 from spyglass.models import Query
 from core.models import NewsStory
+from spyglass.models import Notification
 
 class ProfileTable(tables.Table):
     id = tables.Column(visible=False)
@@ -13,8 +14,8 @@ class ProfileTable(tables.Table):
         attrs = {'class': 'table table-striped table-bordered'}
 
 class NotificationTable(tables.Table):
-    id = tables.Column(visible=False)
 
     class Meta:
-        model = Simple
+        model = Notification
         attrs = {'class': 'table table-striped table-bordered'}
+        fields = ("result", "time")
